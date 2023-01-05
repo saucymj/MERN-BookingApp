336a99bf32298bfb3ca3a20ac9498c1fab0c502d
reyno405 <reyno405@gmail.com>
2023-01-05 18:41:25 -0500

    update to resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,7 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    
+}
